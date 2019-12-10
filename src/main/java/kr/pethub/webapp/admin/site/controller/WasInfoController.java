cc7064cc8b0bad8784ce596222f7a59c9d8156b5
JS Park <parkjjss@gmail.com>
2019-12-10 11:45:54 +0900

    ..

--- src/main/java/kr/pethub/webapp/admin/site/controller/WasInfoController.java
+++ src/main/java/kr/pethub/webapp/admin/site/controller/WasInfoController.java
@@ -1,17 +1,22 @@
 package kr.pethub.webapp.admin.site.controller;
 
+import java.util.List;
+import java.util.Map;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import kr.pethub.webapp.admin.site.service.WasInfoService;
 
 
-@Controller
-@RequestMapping("/wasInfo")
+@RestController
+@RequestMapping("/adm")
 public class WasInfoController{
 	
 	Logger logger = LoggerFactory.getLogger(getClass());
@@ -19,28 +24,24 @@ public class WasInfoController{
 	@Autowired
 	WasInfoService wasInfo;
 
-	@ResponseBody
-	@RequestMapping(value="/cpu")
-	public void cpu() {
-		System.out.println("CPU uage : " + wasInfo.getCpuInfo());
+	@RequestMapping(value="/wasInfo/cpu", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String,Object> cpu() {
+		 return wasInfo.getCpuInfo();
 	} 
 	
-	@ResponseBody
-	@RequestMapping(value="/mem")
-	public void mem() {
-		System.out.println("Memory uage : " + wasInfo.getMemIfo());
+	@RequestMapping(value="/wasInfo/mem", produces = MediaType.APPLICATION_JSON_VALUE)
+	public List<Map<String,Object>> mem() {
+		return wasInfo.getMemIfo();
 	} 
 	
-	@ResponseBody
-	@RequestMapping(value="/jdbc")
-	public void jdbc() {
-		System.out.println("JDBC Pool : " + wasInfo.getJdbcPool());
+	@RequestMapping(value="/wasInfo/jdbc", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String,Object> jdbc() {
+		return wasInfo.getJdbcPool();
 	} 
 	
-	@ResponseBody
-	@RequestMapping(value="/thread")
-	public void thread() {
-		System.out.println("THREAD : " + wasInfo.getThread());
+	@RequestMapping(value="/wasInfo/thread", produces = MediaType.APPLICATION_JSON_VALUE)
+	public Map<String,Object> thread() {
+		return wasInfo.getThread();
 	} 
 	
 	
--- src/main/java/kr/pethub/webapp/admin/site/service/WasInfoService.java
+++ src/main/java/kr/pethub/webapp/admin/site/service/WasInfoService.java
@@ -1,7 +1,9 @@
 package kr.pethub.webapp.admin.site.service;
 
 import java.lang.management.MemoryUsage;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.management.MBeanServerConnection;
@@ -9,6 +11,8 @@ import javax.management.ObjectName;
 import javax.management.openmbean.CompositeData;
 import javax.management.remote.JMXConnector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,30 +22,47 @@ import kr.pethub.core.utils.SysUtil;
 @SuppressWarnings("restriction")
 public class WasInfoService {
 	
+	Logger logger = LoggerFactory.getLogger(getClass());
 	
-	//@Autowired
+	
+	@Autowired
 	private JMXConnector jmxConnector;
 	
-	public String getCpuInfo() {
+	/**
+	 * CPU 사용량
+	 * @return
+	 */
+	public Map<String,Object>  getCpuInfo() {
 		
 		String value = "0";
+		Map<String,Object> map = new HashMap<String, Object>();
+		
 		
 		try {
 
 			MBeanServerConnection mBeanServerConnection = jmxConnector.getMBeanServerConnection();
 			ObjectName objectName = new ObjectName("java.lang:type=OperatingSystem");
-//			System.out.println("ProcessCpuLoad : " + mBeanServerConnection.getAttribute(objectName,	"ProcessCpuLoad"));
+			
+//			logger.info("ProcessCpuLoad : " + mBeanServerConnection.getAttribute(objectName,	"ProcessCpuLoad"));
+			
 			value = String.format("%.1f", (Double)mBeanServerConnection.getAttribute(objectName,"ProcessCpuLoad") * 100 );
+			
+			map.put("cpu",value);
+			
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	     
-	     return value;
+	     return map;
 	}
-	
-	public Map<String,Object> getMemIfo() {
+	 
+	/**
+	 * 메모리 사용량
+	 * @return
+	 */
+	public List<Map<String,Object>> getMemIfo() {
 		
-		Map<String,Object> map = new HashMap<String, Object>();
+		List<Map<String,Object>> list = new  ArrayList<Map<String,Object>>();
 		
 		try {
 			
@@ -49,26 +70,37 @@ public class WasInfoService {
 			ObjectName objectName = new ObjectName("java.lang:type=Memory");
 			MemoryUsage memoryUsage = MemoryUsage.from((CompositeData) mBeanServerConnection.getAttribute(objectName,	"HeapMemoryUsage"));
 			
-//			System.out.println("HeapMemoryUsage-init : \t"+ SysUtil.getFileSize(memoryUsage.getInit()));
-//			System.out.println("HeapMemoryUsage-max : \t"+ SysUtil.getFileSize(memoryUsage.getMax()));
-//			System.out.println("HeapMemoryUsage-used : \t"+ SysUtil.getFileSize(memoryUsage.getUsed()));
-//			System.out.println("HeapMemoryUsage-committed = "+ SysUtil.getFileSize(memoryUsage.getCommitted()));
+//			logger.info("HeapMemoryUsage-init : \t"+ SysUtil.getFileSize(memoryUsage.getInit()));
+//			logger.info("HeapMemoryUsage-max : \t"+ SysUtil.getFileSize(memoryUsage.getMax()));
+//			logger.info("HeapMemoryUsage-used : \t"+ SysUtil.getFileSize(memoryUsage.getUsed()));
+//			logger.info("HeapMemoryUsage-committed = "+ SysUtil.getFileSize(memoryUsage.getCommitted()));
 			
-			map.put("INIT", SysUtil.getFileSize(memoryUsage.getInit()));
-			map.put("MAX", SysUtil.getFileSize(memoryUsage.getMax()));
-			map.put("USED", SysUtil.getFileSize(memoryUsage.getUsed()));
-			map.put("COMMITTED", SysUtil.getFileSize(memoryUsage.getCommitted()));
-
+			Map<String,Object> map1 = new HashMap<String, Object>();
+			map1.put("Init", memoryUsage.getInit());
+			map1.put("Max", memoryUsage.getMax());
+			map1.put("Used", memoryUsage.getUsed());
+			map1.put("Committed", memoryUsage.getCommitted());
+			list.add(map1);
+			
+			Map<String,Object> map2 = new HashMap<String, Object>();
+			map2.put("Init", SysUtil.getFileSize(memoryUsage.getInit()));
+			map2.put("Max", SysUtil.getFileSize(memoryUsage.getMax()));
+			map2.put("Used", SysUtil.getFileSize(memoryUsage.getUsed()));
+			map2.put("Committed", SysUtil.getFileSize(memoryUsage.getCommitted()));
+			list.add(map2);
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		return map;
+		return list;
 	     
 	}
 	
-	
+	/**
+	 * JDBC 커넥션
+	 * @return
+	 */
 	public Map<String,Object> getJdbcPool() {
 		
 		Map<String,Object> map = new HashMap<String, Object>();
@@ -78,9 +110,9 @@ public class WasInfoService {
 			MBeanServerConnection mBeanServerConnection = jmxConnector.getMBeanServerConnection();
 			ObjectName objectName = new ObjectName("kr.pethub.core.configuration.beans:type=DBCP");
 //			
-//			System.out.println("MaxTotal : " + mBeanServerConnection.getAttribute(objectName,	"MaxTotal"));
-//			System.out.println("NumActive : " +mBeanServerConnection.getAttribute(objectName,	"NumActive"));
-//			System.out.println("NumIdle : " +mBeanServerConnection.getAttribute(objectName,	"NumIdle"));
+//			logger.info("MaxTotal : " + mBeanServerConnection.getAttribute(objectName,	"MaxTotal"));
+//			logger.info("NumActive : " +mBeanServerConnection.getAttribute(objectName,	"NumActive"));
+//			logger.info("NumIdle : " +mBeanServerConnection.getAttribute(objectName,	"NumIdle"));
 			
 			map.put("MaxTotal", mBeanServerConnection.getAttribute(objectName,"MaxTotal"));
 			map.put("NumActive", mBeanServerConnection.getAttribute(objectName,"NumActive"));
@@ -95,6 +127,10 @@ public class WasInfoService {
 	     
 	}
 	
+	/**
+	 * 쓰레드
+	 * @return
+	 */
 	public Map<String,Object> getThread() {
 		
 		Map<String,Object> map = new HashMap<String, Object>();
@@ -105,9 +141,9 @@ public class WasInfoService {
 			ObjectName objectName = new ObjectName("java.lang:type=Threading");
 			
 			
-//			System.out.println("PeakThreadCount : " + mBeanServerConnection.getAttribute(objectName,	"PeakThreadCount"));
-//			System.out.println("ThreadCount : " + mBeanServerConnection.getAttribute(objectName,	"ThreadCount"));
-//			System.out.println("TotalStartedThreadCount : " + mBeanServerConnection.getAttribute(objectName,	"TotalStartedThreadCount"));
+//			logger.info("PeakThreadCount : " + mBeanServerConnection.getAttribute(objectName,	"PeakThreadCount"));
+//			logger.info("ThreadCount : " + mBeanServerConnection.getAttribute(objectName,	"ThreadCount"));
+//			logger.info("TotalStartedThreadCount : " + mBeanServerConnection.getAttribute(objectName,	"TotalStartedThreadCount"));
 			
 			
 			map.put("PeakThreadCount", mBeanServerConnection.getAttribute(objectName,"PeakThreadCount"));
